23e0bbf8cbcca7d2c6cc775b90dfccdac4486d24
robertsonics <jamie@robertsonics.com>
2014-05-11 09:42:04 -0700

    Added functions for track fades and crossfades, as well as for loading and resuming multiple tracks in sync.
    Signed-off-by: robertsonics <jamie@robertsonics.com>

--- wavTrigger.cpp
+++ wavTrigger.cpp
@@ -30,6 +30,12 @@ void wavTrigger::trackPlayPoly(int trk) {
 }
 
 // **************************************************************
+void wavTrigger::trackLoad(int trk) {
+  
+  trackControl(trk, TRK_LOAD);
+}
+
+// **************************************************************
 void wavTrigger::trackStop(int trk) {
 
   trackControl(trk, TRK_STOP);
@@ -86,6 +92,19 @@ byte txbuf[5];
 }
 
 // **************************************************************
+void wavTrigger::resumeAllInSync(void) {
+
+byte txbuf[5];
+
+  txbuf[0] = 0xf0;
+  txbuf[1] = 0xaa;
+  txbuf[2] = 0x05;
+  txbuf[3] = CMD_RESUME_ALL_SYNC;
+  txbuf[4] = 0x55;
+  WTSerial.write(txbuf, 5);
+}
+
+// **************************************************************
 void wavTrigger::trackGain(int trk, int gain) {
 
 byte txbuf[9];
@@ -102,6 +121,71 @@ unsigned short vol;
   txbuf[7] = (byte)(vol >> 8);
   txbuf[8] = 0x55;
   WTSerial.write(txbuf, 9);
+}
+
+// **************************************************************
+void wavTrigger::trackFade(int trk, int gain, int time, bool stopFlag) {
+
+byte txbuf[12];
+unsigned short vol;
+
+  txbuf[0] = 0xf0;
+  txbuf[1] = 0xaa;
+  txbuf[2] = 0x0c;
+  txbuf[3] = CMD_TRACK_FADE;
+  txbuf[4] = (byte)trk;
+  txbuf[5] = (byte)(trk >> 8);
+  vol = (unsigned short)gain;
+  txbuf[6] = (byte)vol;
+  txbuf[7] = (byte)(vol >> 8);
+  txbuf[8] = (byte)time;
+  txbuf[9] = (byte)(time >> 8);
+  txbuf[10] = stopFlag;
+  txbuf[11] = 0x55;
+  WTSerial.write(txbuf, 12);
+}
+
+// **************************************************************
+void wavTrigger::trackCrossFade(int trkFrom, int trkTo, int gain, int time) {
+
+byte txbuf[12];
+unsigned short vol;
+
+  // Start the To track with -40 dB gain
+  trackGain(trkTo, -40);
+  trackPlayPoly(trkTo);
+
+  // Start a fade-in to the target volume
+  txbuf[0] = 0xf0;
+  txbuf[1] = 0xaa;
+  txbuf[2] = 0x0c;
+  txbuf[3] = CMD_TRACK_FADE;
+  txbuf[4] = (byte)trkTo;
+  txbuf[5] = (byte)(trkTo >> 8);
+  vol = (unsigned short)gain;
+  txbuf[6] = (byte)vol;
+  txbuf[7] = (byte)(vol >> 8);
+  txbuf[8] = (byte)time;
+  txbuf[9] = (byte)(time >> 8);
+  txbuf[10] = 0x00;
+  txbuf[11] = 0x55;
+  WTSerial.write(txbuf, 12);
+
+  // Start a fade-out on the From track
+  txbuf[0] = 0xf0;
+  txbuf[1] = 0xaa;
+  txbuf[2] = 0x0c;
+  txbuf[3] = CMD_TRACK_FADE;
+  txbuf[4] = (byte)trkFrom;
+  txbuf[5] = (byte)(trkFrom >> 8);
+  vol = (unsigned short)-40;
+  txbuf[6] = (byte)vol;
+  txbuf[7] = (byte)(vol >> 8);
+  txbuf[8] = (byte)time;
+  txbuf[9] = (byte)(time >> 8);
+  txbuf[10] = 0x01;
+  txbuf[11] = 0x55;
+  WTSerial.write(txbuf, 12);
 }
 
 
--- wavTrigger.h
+++ wavTrigger.h
@@ -14,9 +14,13 @@
 // --------  -----------
 //
 // 02/22/14  First version created.
-//           LIMITATIONS: Hard-coded for the YUN to use the
-//           AltSoftwareSerial Library. Also only supports
-//           commands TO the WAV Trigger. Will fix this things.
+//           LIMITATIONS: Hard-coded for AltSoftwareSerial Library.
+//           Also only supports commands TO the WAV Trigger. Will
+//           fix these things.
+//
+// 05/10/14  Tested with UNO. Added new functions for fades, cross-
+//           fades and starting multiple tracks in sample sync.
+
 
 #ifndef WAVTRIGGER_H
 #define WAVTRIGGER_H
@@ -24,6 +28,8 @@
 #define CMD_TRACK_CONTROL	3
 #define CMD_STOP_ALL		4
 #define CMD_TRACK_VOLUME	8
+#define CMD_TRACK_FADE		10
+#define CMD_RESUME_ALL_SYNC	11
 
 #define TRK_PLAY_SOLO	0
 #define TRK_PLAY_POLY	1
@@ -32,6 +38,7 @@
 #define TRK_STOP		4
 #define TRK_LOOP_ON		5
 #define TRK_LOOP_OFF	6
+#define TRK_LOAD		7
 
 #include "../AltSoftSerial/AltSoftSerial.h"
 
@@ -42,13 +49,17 @@ public:
 	~wavTrigger() {;}
 	void start(void);
 	void stopAllTracks(void);
+	void resumeAllInSync(void);
 	void trackPlaySolo(int trk);
 	void trackPlayPoly(int trk);
+	void trackLoad(int trk);
 	void trackStop(int trk);
 	void trackPause(int trk);
 	void trackResume(int trk);
 	void trackLoop(int trk, bool enable);
 	void trackGain(int trk, int gain);
+	void trackFade(int trk, int gain, int time, bool stopFlag);
+	void trackCrossFade(int trkFrom, int trkTo, int gain, int time);
 private:
 	void trackControl(int trk, int code);
 
