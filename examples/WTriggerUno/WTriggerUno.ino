7e449d0f9928fb197e40604100ca161b7c51d802
robertsonics <jamie@robertsonics.com>
2015-04-25 11:46:25 -0700

    Updated Uno example
    Signed-off-by: robertsonics <jamie@robertsonics.com>

--- examples/WTriggerUno/WTriggerUno.ino
+++ examples/WTriggerUno/WTriggerUno.ino
@@ -25,9 +25,13 @@
 //
 //    5V   <------> 5V
 //
-// 4) Make sure you have tracks 1 through 4 installed on the WAV Trigger. This
-//    means compatible (16-bit, stereo, 44.1kHz) wav files named 001xxx.wav
-//    through 004xxx.wav.
+// 4) Download and install the demo wav files onto the WAV Trigger's microSD
+//    card. You can find them here:
+//
+//
+//    You can certainly use your own tracks instead, although the demo may
+//    not make as much sense. If you do, make sure your tracks are at least 10
+//    to 20 seconds long and have no silence at the start of the file.
 
 #include "Metro.h"
 #include "AltSoftSerial.h"    // Arduino build environment requires this
@@ -38,7 +42,7 @@
 wavTrigger wTrig;             // Our WAV Trigger object
 
 Metro gLedMetro(500);         // LED blink interval timer
-Metro gWTrigMetro(5000);      // WAV Trigger state machine interval timer
+Metro gWTrigMetro(6000);      // WAV Trigger state machine interval timer
 
 byte gLedState = 0;           // LED State
 int  gWTrigState = 0;         // WAV Trigger state
@@ -71,7 +75,7 @@ void setup() {
 
 // ****************************************************************************
 // This program uses a Metro timer to create a state machine that steps
-//  through states at 5 second intervals - you can change this rate above.
+//  through states at 8 second intervals - you can change this rate above.
 //  Each state will execute a new wavTrigger command.
 
 void loop() {  
@@ -82,34 +86,68 @@ void loop() {
       
       switch (gWTrigState) {
   
-          // Fade in track 1    
+          // Demonstrates how to fade in a music track 
           case 0:
-              wTrig.trackGain(1, -40);              // Preset Track 1 gain to -40dB
-              wTrig.trackPlayPoly(1);               // Start Track 1
-              wTrig.trackFade(1, 0, 2000, 0);       // Fade Track 1 to 0dB
+              wTrig.masterGain(0);                  // Reset the master gain to 0dB                 
+              wTrig.trackGain(2, -40);              // Preset Track 2 gain to -40dB
+              wTrig.trackPlayPoly(2);               // Start Track 2
+              wTrig.trackFade(2, 0, 2000, 0);       // Fade Track 2 to 0dB
           break;
 
+          // Demonstrates how to cross-fade music tracks
           case 1:
-              wTrig.trackCrossFade(1, 2, 0, 2000);  // Cross-fade Track 1 to Track 2
+              wTrig.trackCrossFade(2, 1, 0, 2000);  // Cross-fade Track 2 to Track 1
           break;
                                  
+          // Fade down music and start looping dialog
           case 2:
-              wTrig.trackPlayPoly(3);               // Start Track 3 poly
+              wTrig.trackFade(1, -6, 500, 0);
+              wTrig.trackPlayPoly(4);               // Start Track 4 poly
+              wTrig.trackLoop(4, 1);                // Enable Track 4 looping
           break;
 
+          // Honk the horn 2 times
           case 3:
-              wTrig.trackFade(3, -50, 5000, 1);     // Fade Track 3 to -50dB and stop
+              wTrig.trackPlayPoly(5);               // Start Track 5 poly
+              delay(500);
+              wTrig.trackStop(5);                   // Stop Track 5
+              delay(250);
+              wTrig.trackPlayPoly(5);               // Start Track 5 poly
+              delay(500);
+              wTrig.trackStop(5);                   // Stop Track 5
           break;
-                    
+          
+          // Fade out and stop dialog
           case 4:
-              wTrig.stopAllTracks();                // Stop all tracks
+              wTrig.trackLoop(4, 0);                // Disable Track 4 looping
+              wTrig.trackFade(4, -50, 5000, 1);     // Fade Track 4 to -50dB and stop
           break;
+          
+          // This demonstrates playing musical instrument samples, as well as pre-loading
+          //  multiple tracks and then starting them in sample sync.
+          case 5:
+              wTrig.masterGain(-8);                 // Lower main volume
+              wTrig.trackPlayPoly(6);               // Play first note
+              delay(500);
+              wTrig.trackPlayPoly(7);               // Play second note
+              delay(500);
+              wTrig.trackPlayPoly(8);               // Play third note
+              delay(500);
+              wTrig.stopAllTracks();                // Stop all
+              delay(500);
+              wTrig.trackLoad(6);                   // Load and pause Track 6
+              wTrig.trackLoad(7);                   // Load and pause Track 7
+              wTrig.trackLoad(8);                   // Load and pause Track 8
+              wTrig.resumeAllInSync();              // Start all in sample sync
+              delay(3000);
+              wTrig.stopAllTracks();                // Stop ll
+          break;        
            
       } // switch
  
       // Increment our state
       gWTrigState++;
-      if (gWTrigState > 4)
+      if (gWTrigState > 5)
           gWTrigState = 0;
           
   } // if (gWTrigState.check() == 1)
